c11aa3ea197dcc91c06519c40b4fdfce60a34c43
SylarK <syst.lucasamado@gmail.com>
2020-08-15 17:30:10 -0300

    Layout ok, trabalhando na lógica para subir os dados para o front end

--- WeatherApp X/scripts.js
+++ WeatherApp X/scripts.js
@@ -1,22 +1,31 @@
-const btn_weather = document.querySelector('#btn-weather');
-const city = document.querySelector('#city');
+const searchbox = document.querySelector('#city');
 const key = 'a36b3b604804a1dc6b5820c8b7911118'; /*ori*/ 
             /*'7b4e840accf560effac0b4981c1438fd'*/
-            
-let result = document.querySelector('#weather');
 
-btn_weather.addEventListener('click', getInfo);
+/*  Adicionando um evento que escutará as teclas pessionadas  */
+/*  Lista com keycodes para cada tecla: http://gcctech.org/csc/javascript/javascript_keycodes.htm */
 
-function getInfo(){
+searchbox.addEventListener('keypress', checkEnter)
 
-    q = city.value;
-    let api = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&appid=" + key;
-    city.value = '';
+function checkEnter(evt){
+
+    if(evt.keyCode == 13){
+        getInfo(searchbox.value)
+    }
+
+}
+
+
+function getInfo(value){
+
+    q = value;
+    let api = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&units=metric&appid=" + key;
+    /*Lembrar de zerar valor do input*/
 
     fetch(api)
         .then(res => res.json())
         .then(data => {
-            init(data)         
+            console.log(data)        
         })  
 }
 
